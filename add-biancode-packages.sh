0797b76d2e3437e4829b15fe756b3fdc00f93bc5
Klaus Landsdorf <klaus.landsdorf@bianco-royal.de>
2019-11-17 21:57:33 +0100

    chore: dev settings bianco

---
+++ add-biancode-packages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ./node_modules
+
+ln -s ~/Development/github/biancode/node-red-contrib-opcua-client-api node-red-contrib-opcua-client-api
---
+++ add-biancoroyal-packages.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd ./node_modules
+
+ln -s ~/Development/github/biancoroyal/node-red-contrib-opcua-server node-red-contrib-opcua-server
+ln -s ~/Development/github/biancoroyal/node-red-contrib-iiot-opcua node-red-contrib-iiot-opcua
+ln -s ~/Development/github/biancoroyal/node-red-contrib-modbus node-red-contrib-modbus
