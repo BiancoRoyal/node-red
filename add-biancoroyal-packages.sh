b5f6331e2873bc65f58d3bbf2e74eddf8e755000
Klaus Landsdorf <klaus.landsdorf@bianco-royal.de>
2019-11-24 09:13:17 +0100

    fix: rm before add link to contribs

--- add-biancoroyal-packages.sh
+++ add-biancoroyal-packages.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-cd ./node_modules
+cd ~/Development/github/BiancoRoyal/node-red/node_modules
 
+# rm node-red-contrib-opcua-server
 # ln -s ~/Development/github/BiancoRoyal/node-red-contrib-opcua-server node-red-contrib-opcua-server
-# ln -s ~/Development/github/BiancoRoyal/node-red-contrib-iiot-opcua node-red-contrib-iiot-opcua
+
+rm node-red-contrib-iiot-opcua
+ln -s ~/Development/github/BiancoRoyal/next/node-red-contrib-iiot-opcua node-red-contrib-iiot-opcua
+
+rm node-red-contrib-modbus
 ln -s ~/Development/github/BiancoRoyal/node-red-contrib-modbus node-red-contrib-modbus
