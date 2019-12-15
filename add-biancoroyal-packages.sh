d6acd2768b4129ad31871c6ddca0df7d9e4015bd
Klaus Landsdorf <klaus.landsdorf@bianco-royal.de>
2019-12-15 09:35:49 +0100

    chore: add contrib packages

--- add-biancoroyal-packages.sh
+++ add-biancoroyal-packages.sh
@@ -2,8 +2,8 @@
 
 cd ~/Development/github/BiancoRoyal/node-red/node_modules
 
-# rm node-red-contrib-opcua-server
-# ln -s ~/Development/github/BiancoRoyal/node-red-contrib-opcua-server node-red-contrib-opcua-server
+rm node-red-contrib-opcua-server
+ln -s ~/Development/github/BiancoRoyal/node-red-contrib-opcua-server node-red-contrib-opcua-server
 
 rm node-red-contrib-iiot-opcua
 ln -s ~/Development/github/BiancoRoyal/next/node-red-contrib-iiot-opcua node-red-contrib-iiot-opcua
